d2c54c49164c5b894041b9fad8234f1e921af1c2
lielbe <lielbe@checkpoint.com>
2024-06-24 17:07:58 +0300

    update ext info and get GW

--- showSimpleGatewayInterfaces.js
+++ showSimpleGatewayInterfaces.js
@@ -5,10 +5,14 @@
 const interfaceIcon =
   "M10.000,8.000 L10.000,7.000 L7.000,7.000 L6.000,7.000 L3.000,7.000 L3.000,8.000 L2.000,8.000 L2.000,7.000 L2.000,6.000 L3.000,6.000 L6.000,6.000 L6.000,5.000 L7.000,5.000 L7.000,6.000 L10.000,6.000 L11.000,6.000 L11.000,7.000 L11.000,8.000 L10.000,8.000 ZM8.000,4.000 L7.000,4.000 L7.000,4.000 L6.000,4.000 L6.000,4.000 L5.000,4.000 C4.448,4.000 4.000,3.552 4.000,3.000 L4.000,1.000 C4.000,0.447 4.448,-0.000 5.000,-0.000 L8.000,-0.000 C8.552,-0.000 9.000,0.447 9.000,1.000 L9.000,3.000 C9.000,3.552 8.552,4.000 8.000,4.000 ZM5.000,10.000 L5.000,12.000 C5.000,12.552 4.552,13.000 4.000,13.000 L1.000,13.000 C0.448,13.000 -0.000,12.552 -0.000,12.000 L-0.000,10.000 C-0.000,9.447 0.448,9.000 1.000,9.000 L1.000,9.000 L1.000,9.000 L4.001,9.000 C4.553,9.001 5.000,9.448 5.000,10.000 ZM9.000,9.000 L9.000,9.000 L9.000,9.000 L12.001,9.000 C12.553,9.001 13.000,9.448 13.000,10.000 L13.000,12.000 C13.000,12.552 12.552,13.000 12.000,13.000 L9.000,13.000 C8.448,13.000 8.000,12.552 8.000,12.000 L8.000,10.000 C8.000,9.447 8.448,9.000 9.000,9.000 Z";
 
+  function onContext(obj) {
+    removeLoader()
+    console.log(obj)
+    initParameters()
+  }
 
   function initParameters() {
 
-    removeLoader()
     const toggleEnableDisable = document.getElementById("toggleEnableDisable");
     const stateEnableDisable = document.getElementById("stateEnableDisable");
     const toggleAction = document.getElementById("toggleAction");
@@ -86,6 +90,5 @@ function showContext() {
   addLoader();
   // send API request
   // smxProxy.sendRequest("get-context", null, "onContext");
-
-  initParameters();
+  smxProxy.sendRequest("get-context", null, "onContext");
 }
