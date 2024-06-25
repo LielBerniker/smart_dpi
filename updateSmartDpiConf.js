9643ed11bac4d088ba50b6054100f8ff21ef317f
lielbe <lielbe@checkpoint.com>
2024-06-25 14:45:19 +0300

    tun malware config update

--- updateSmartDpiConf.js
+++ updateSmartDpiConf.js
@@ -5,9 +5,30 @@
 const interfaceIcon =
   "M10.000,8.000 L10.000,7.000 L7.000,7.000 L6.000,7.000 L3.000,7.000 L3.000,8.000 L2.000,8.000 L2.000,7.000 L2.000,6.000 L3.000,6.000 L6.000,6.000 L6.000,5.000 L7.000,5.000 L7.000,6.000 L10.000,6.000 L11.000,6.000 L11.000,7.000 L11.000,8.000 L10.000,8.000 ZM8.000,4.000 L7.000,4.000 L7.000,4.000 L6.000,4.000 L6.000,4.000 L5.000,4.000 C4.448,4.000 4.000,3.552 4.000,3.000 L4.000,1.000 C4.000,0.447 4.448,-0.000 5.000,-0.000 L8.000,-0.000 C8.552,-0.000 9.000,0.447 9.000,1.000 L9.000,3.000 C9.000,3.552 8.552,4.000 8.000,4.000 ZM5.000,10.000 L5.000,12.000 C5.000,12.552 4.552,13.000 4.000,13.000 L1.000,13.000 C0.448,13.000 -0.000,12.552 -0.000,12.000 L-0.000,10.000 C-0.000,9.447 0.448,9.000 1.000,9.000 L1.000,9.000 L1.000,9.000 L4.001,9.000 C4.553,9.001 5.000,9.448 5.000,10.000 ZM9.000,9.000 L9.000,9.000 L9.000,9.000 L12.001,9.000 C12.553,9.001 13.000,9.448 13.000,10.000 L13.000,12.000 C13.000,12.552 12.552,13.000 12.000,13.000 L9.000,13.000 C8.448,13.000 8.000,12.552 8.000,12.000 L8.000,10.000 C8.000,9.447 8.448,9.000 9.000,9.000 Z";
 
-  function onContext(obj) {
+  class GatewayConfigInfo {
+    constructor(name, isEnabled, actionMode, threshold) {
+        this.name = name;
+        this.isEnabled = isEnabled;
+        this.actionMode = actionMode;
+        this.threshold = threshold;
+    }
+}
+
+const smartDpiConfig = "python3 smart_dpi_config_update.pyc"
+
+
+  function onCommit(value) {
     removeLoader()
-    initParameters(obj)
+    var result = JSON.stringify(value);
+    //do something with the JSON formatted context
+  }
+
+  function runUpdateConfigOnGW(gatewayInfo) {
+
+    const updateConfigCli = smartDpiConfig + " " + gatewayInfo.isEnabled + " " + gatewayInfo.actionMode + " " + gatewayInfo.threshold
+    const mgmtCli = `mgmt_cli run-script script-name "smart_dpi_config_update" script "${updateConfigCli}" targets.1 "${gatewayInfo.name}" --format json`;
+    //request to commit changes
+    smxProxy.sendRequest("request-commit", {"commands" : [mgmtCli]}, "onCommit");
   }
 
   function initParameters(obj) {
@@ -50,13 +71,11 @@ const interfaceIcon =
           return;
       }
 
-      var type = obj.event.objects[0]["type"];
       var name = obj.event.objects[0]["name"];
 
-      // Join the types into a single string, separated by commas
-      var infoString = type + ", " + name
-
-      alert(infoString);
+      addLoader()
+      gatewayInfo = new GatewayConfigInfo(name, isEnabled, actionMode, threshold);
+      runUpdateConfigOnGW(gatewayInfo)
       // Display the collected data
       console.log({
           enabled: isEnabled,
@@ -88,6 +107,12 @@ function removeLoader() {
   var text = document.getElementById("loader-text");
   document.body.removeChild(text);
 }
+
+function onContext(obj) {
+  removeLoader()
+  initParameters(obj)
+}
+
 
 /*
  * Send API request 'get-context' (get-context return JSON object of extension location context).
